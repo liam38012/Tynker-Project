ee8cb998e033822f8664d0bf98351343bf6767fb
liam38012 <liam38011@gmail.com>
2022-02-15 08:30:31 +0100

    Tynker Project

---
+++ App.js
@@ -0,0 +1,56 @@
+import Header from './Components/next'
+import Next from './Components/header'
+import Third from './Components/third'
+import Fourth from './Components/fourth'
+ import Fifth from './Components/fifth'
+ import Sixth from './Components/sixth'
+ import Seventh from './Components/seventh'
+ import Eight from './Components/eight'
+ import Last from './Components/last'
+
+
+function App(){
+  return (
+    <div>
+      <Next/>
+      <Header/>
+      <Third/>
+      <Fourth/>
+      <Fifth/>
+      <Sixth/>
+      <Seventh/>
+      <Eight/>
+      <Last/> 
+    </div>
+      
+
+    
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <Router>
+        <Header/>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          < Route path='/about' element={<About />} />
+          <Route path='/contact'  element={<Contact />} />
+        </Routes>
+      </Router> */}
+      {/* <Card/>  */}
+      
